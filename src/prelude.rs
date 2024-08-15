be11b7cf21436a6877827844e748d581d291a030
Lautaro <lautaro9galante@gmail.com>
2024-08-15 00:11:04 -0300

    adding prelude file

---
+++ src/prelude.rs
@@ -0,0 +1,8 @@
+pub use crate::config::config::Config;
+pub use crate::icons::selected_icons::SelectedIcons;
+pub use crate::icons::icons::Icons;
+pub use crate::errors::error::CliError;
+pub use crate::utils::*;
+pub use chrono::Local;
+pub use serde::Deserialize;
+pub use std::{env, fs, io, num, path::Path, process::Command, thread, time};
